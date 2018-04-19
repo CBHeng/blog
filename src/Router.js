e4f53f5afd339963da0dd6bb61bd82ecccd53edd
Zcon-ChenBoHeng <zcon.chenboheng@gmail.com>
2018-04-19 19:50:26 +0800

    reactjs gh-page config

--- src/Router.js
+++ src/Router.js
@@ -1,13 +1,10 @@
 import React, { Component } from 'react';
-import { BrowserRouter, Route, Link } from 'react-router-dom'
 import Home from './Home.js'
 
 class Router extends Component {
   render() {
     return (
-      <BrowserRouter>
-          <Route exact path="/" component={Home} />
-      </BrowserRouter>
+     <Home />
     );
   }
 }
